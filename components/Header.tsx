c828d4d43350b23ecd54908e2c0c9c09ed169186
Sandhya <ssankaran3@horizon.csueastbay.edu>
2024-07-01 10:49:10 -0700

    created initial files

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+function Header() {
+  return (
+    <div>
+      <Button>sign in</Button>
+    </div>
+  )
+}
+
+export default Header
